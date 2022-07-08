7095aa978e8bdda8cd70e19be367649e132722d5
maanvivarma <85298661+maanvivarma@users.noreply.github.com>
2022-07-08 18:04:51 +0530

    Add files via upload

---
+++ screens/Transaction.js
@@ -0,0 +1,30 @@
+import React,{Component} from "react";
+import {View,Text,StyleSheet} from "react-native";
+
+export default class TransactionScreen extends Component{
+    render(){
+        return(
+            <Veiw style={styles.container}>
+                
+                <Text style={styles.text}>
+                    Transaction Screen
+                </Text>
+            </Veiw>
+        )
+    }
+}
+
+const styles=StyleSheet.create({
+    container:
+     {
+         flex: 1,
+         justifyContent: "center",
+        alignItems: "center", 
+        backgroundColor: "#5653D4" 
+    },
+     text: 
+     { color: "#ffff",
+      fontSize: 30
+     }
+
+})
